e9cc76083914ab15aa2121d3e1b65aa62c8ac1aa
yinshuennn <122251607+yinshuennn@users.noreply.github.com>
2023-05-23 11:48:53 +0800

    Add screens of app

---
+++ index.js
@@ -0,0 +1,15 @@
+import Launch from "./Launch"
+import Login from "./Login"
+import SignUp from "./SignUp"
+import Home from "./Home"
+import ForgotPassword from "./ForgotPassword"
+import SendEmail from "./SendEmail"
+
+export {
+    Launch,
+    Login,
+    SignUp,
+    Home,
+    ForgotPassword,
+    SendEmail,
+};
