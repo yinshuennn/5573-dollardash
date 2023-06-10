996e13e397c0a8121fba382bc0ee98515959f715
yinshuennn <122251607+yinshuennn@users.noreply.github.com>
2023-06-11 00:58:51 +0800

    Update App.js

--- App.js
+++ App.js
@@ -5,8 +5,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LaunchScreen from './screens/Launch';
 import LoginScreen from './screens/Login';
+import ForgotPassword from './screens/ForgotPassword';
 import SignUpScreen from './screens/SignUp';
-import HomeScreen from './screens/Home';
+import Home from './screens/Home';
+import AddNewExpense from './screens/AddNewExpense';
+import Profile from './screens/Profile';
+import EditEmail from './screens/EditEmail';
+import ChangePassword from './screens/ChangePassword';
+import Notifications from './screens/Notifications';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,8 +22,14 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen options={{ headerShown: false }} name="Launch" component={LaunchScreen} />
         <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+        <Stack.Screen options={{ headerShown: false }} name="ForgotPassword" component={ForgotPassword} />
         <Stack.Screen options={{ headerShown: false }} name="SignUp" component={SignUpScreen} />
-        <Stack.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
+        <Stack.Screen options={{ headerShown: false }} name="Home" component={Home} />
+        <Stack.Screen options={{ headerShown: false }} name="AddNewExpense" component={AddNewExpense} />
+        <Stack.Screen options={{ headerShown: false }} name="Profile" component={Profile} />
+        <Stack.Screen options={{ headerShown: false }} name="EditEmail" component={EditEmail} />
+        <Stack.Screen options={{ headerShown: false }} name="ChangePassword" component={ChangePassword} />
+        <Stack.Screen options={{ headerShown: false }} name="Notifications" component={Notifications} />
       </Stack.Navigator>
     </NavigationContainer>
   );
