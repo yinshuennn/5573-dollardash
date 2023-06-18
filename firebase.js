0a71bb1264d1878a44485cb0b778e1c523bf6366
BrendaTan29 <100470975+BrendaTan29@users.noreply.github.com>
2023-06-18 23:11:43 +0800

    updated firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,5 @@
-// Import the functions you need from the SDKs you need
-// import * as firebase from "firebase";
 import firebase from "firebase/compat";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCkF5HnzoHw48U5dKfRwdWOaYDCDGMFVos",
   authDomain: "dollardash-4491f.firebaseapp.com",
@@ -14,7 +9,6 @@ const firebaseConfig = {
   appId: "1:54139144467:web:a4651b76e684f432a2f2e6"
 };
 
-// Initialize Firebase
 let app;
 
 if (firebase.apps.length === 0) {
@@ -24,5 +18,6 @@ if (firebase.apps.length === 0) {
 }
 
 const auth = firebase.auth()
+const db = firebase.firestore()
 
-export { auth };
+export { auth, db, firebase };
